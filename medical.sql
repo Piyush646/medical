a2b7750d3470d4a0597da92191c9f754b5c8adb9
vansh10patpatia <vansh10patpatia@gmail.com>
2021-03-29 04:34:16 +0530

    initial comment

--- medical.sql
+++ medical.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2021 at 05:31 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.8
+-- Generation Time: Mar 29, 2021 at 01:03 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,28 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category` text DEFAULT NULL,
+  `cat_logo` text DEFAULT NULL,
+  `cat_img` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `category`, `cat_logo`, `cat_img`) VALUES
+(1, 'Face Masks', 'icon_masks.png', 'masks.png'),
+(2, 'Shoe Covers', 'icon_shoes.png', 'shoes.png'),
+(3, 'Mob Caps', 'icon_mob.png', 'mob.png'),
+(4, 'Incontinence', 'icon_incon.png', 'incontinence.png'),
+(5, 'Gloves', 'icon_gloves.png', 'gloves.png'),
+(6, 'Oral Swaps', 'icon_oral.png', 'oral.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_slider`
+--
+
+CREATE TABLE `home_slider` (
+  `id` bigint(50) NOT NULL,
+  `heading` text NOT NULL,
+  `sub_heading` text NOT NULL,
+  `image` text NOT NULL,
+  `link` text NOT NULL,
+  `sort_order` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `home_slider`
+--
+
+INSERT INTO `home_slider` (`id`, `heading`, `sub_heading`, `image`, `link`, `sort_order`) VALUES
+(1, 'Ezy-Aid 70% Isopropyl Alcohol Pre-Injection Swabs (100pk) ', 'Ezy-Aid 70% Isopropyl Alcohol Pre-Injection Swabs (100pk)\r\n\r\nPre-injection - Skin Cleansing Swabs\r\n100 per box\r\n70% Isopropyl Alcohol\r\nApprox Size of Swabs: 3x3cm', 'uploads/1616319283_41UC0vv2ZjL._AC_.jpg', 'https://dubuddy.in/medical/index', 1),
+(2, 'Dermastrip Washproof Plasters - Pack of 100-7 Sizes Assorted', 'Dermastrip Washproof Plasters - Pack of 100-7 Sizes Assorted\r\n\r\nDermastrip Hypo-Allergenic Washproof Plasters\r\n7 Types (Sizes) of Plasters Included in a divider tray\r\nBox of 100\r\nIndividually wrapped and Sterile', 'uploads/1616319295_download.jpeg', 'https://dubuddy.in/medical/index', 1),
+(3, 'Ezy-Aid Box 24 Zinc Oxide Premium Tape', 'Ezy-Aid Box 24 Zinc Oxide Premium Tape\r\n\r\nEzy-Aid Premium Zinc Oxide Tape\r\nManufactured with High Quality Materials to provide with a fantastic Combination of Extreme Strength yet Easy to Tear\r\nLatex Free. Ideal for use in Sporting Activities or to Secure Dressings / Medical Applications\r\nBox Pack of 24 -- Size: 1.25cm x 10Meters each roll\r\nSuper Saver Bulk Pack', 'uploads/1616319214_41YPo1ePWsL._AC_.jpg', 'https://dubuddy.in/medical/index', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `name` text DEFAULT NULL,
-  `price` bigint(20) DEFAULT NULL,
+  `price` text DEFAULT NULL,
   `dis` text DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
-  `id` bigint(20) NOT NULL,
+  `id` bigint(50) NOT NULL,
   `code` bigint(50) NOT NULL,
-  `new` bigint(20) NOT NULL
+  `new` bigint(20) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `short_des` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`name`, `price`, `dis`, `status`, `id`, `code`, `new`) VALUES
-('x ray', 100000000, 'for chest related', 2, 21, 345667097, 1),
-('aysgfe', 1200000, 'asfgylfs as;ga;gu faus;fg', 2, 22, 119117, 1),
-('1234567', 123456, '1234567', 2, 23, 123456, 1),
-('ecg', 123243342, 'chest', 1, 24, 1, 0);
+INSERT INTO `product` (`name`, `price`, `dis`, `status`, `id`, `code`, `new`, `time_stamp`, `short_des`) VALUES
+('Dermastrip Washproof Plasters - Pack of 100-7 Sizes Assorted', '5', 'Dermastrip Washproof Plasters - Pack of 100-7 Sizes Assorted\r\n\r\n\r\nDermastrip Hypo-Allergenic Washproof Plasters\r\n7 Types (Sizes) of Plasters Included in a divider tray\r\nBox of 100\r\nIndividually wrapped and Sterile', 2, 21, 345667097, 1, '2021-03-21 10:08:25', 'Dermastrip Washproof Plasters - Pack of 100-7 Sizes Assorted'),
+('Ezy-Aid 70% Isopropyl Alcohol Pre-Injection Swabs (100pk)', '4', 'Ezy-Aid 70% Isopropyl Alcohol Pre-Injection Swabs (100pk)\r\n\r\nPre-injection - Skin Cleansing Swabs\r\n100 per box\r\n70% Isopropyl Alcohol\r\nApprox Size of Swabs: 3x3cm', 2, 22, 119117, 1, '2021-03-21 10:10:10', 'Ezy-Aid 70% Isopropyl Alcohol Pre-Injection Swabs (100pk)'),
+('1234567', '123456', '1234567sad', 0, 23, 123456, 0, '2021-03-21 10:10:23', '');
 
 -- --------------------------------------------------------
 
@@ -65,12 +114,8 @@ CREATE TABLE `product_img` (
 --
 
 INSERT INTO `product_img` (`id`, `p_id`, `img`) VALUES
-(9, 22, 'http://localhost/medical/admin/uploads/vlcsnap-2021-01-16-01h36m26s617.1614251480.png'),
-(11, 23, 'http://localhost/medical/admin/uploads/Screenshot (11).1614338993.png'),
-(12, 21, 'http://localhost/medical/admin/uploads/vlcsnap-2021-01-16-01h36m26s617.1614427983.png'),
-(13, 21, 'http://localhost/medical/admin/uploads/vlcsnap-2021-01-16-01h36m26s617.1614436201.png'),
-(14, 21, 'http://localhost/medical/admin/uploads/vlcsnap-2021-01-16-01h36m26s617.1614436388.png'),
-(16, 24, 'http://localhost/medical/admin/uploads/vlcsnap-2021-01-16-01h36m26s504.1614516698.png');
+(12, 21, 'https://dubuddy.in/medical/admin/uploads/download.1616321281.jpeg'),
+(13, 22, 'https://dubuddy.in/medical/admin/uploads/41UC0vv2ZjL._AC_.1616321410.jpg');
 
 -- --------------------------------------------------------
 
@@ -79,7 +124,7 @@ INSERT INTO `product_img` (`id`, `p_id`, `img`) VALUES
 --
 
 CREATE TABLE `quote` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(50) NOT NULL,
   `name` text DEFAULT NULL,
   `email` text DEFAULT NULL,
   `subject` text DEFAULT NULL,
@@ -99,7 +144,12 @@ INSERT INTO `quote` (`id`, `name`, `email`, `subject`, `msg`) VALUES
 (31, 'cdzczd', 'dvzvjlzfjl@36', 'yifvifo', 'dhkv bhozdvbozbfol'),
 (37, 'gggfg', 'esf@4335', 'asgthj', 'gtjtucgjjhvhk'),
 (38, 'gggfg', 'esf@4335', 'asgthj', 'gtjtucgjjhvhk'),
-(40, 'gggfg', 'esf@4335', 'asgthj', 'gtjtucgjjhvhk');
+(40, 'gggfg', 'esf@4335', 'asgthj', 'gtjtucgjjhvhk'),
+(43, 'Pancham', 'panchamsheoran@gmail.com', 'Login problem ', 'test message'),
+(44, 'Test', 'test@gmail.com', 'query', 'test message'),
+(45, 'Hi', 'hi@xyz', 'please call H', 'please call H'),
+(46, 'Hi', 'hi@xyz.com', 'please call H', 'please call H'),
+(47, 'XYZ', 'xyz@gmail.com', 'quite for face mask', 'Hi -sds');
 
 -- --------------------------------------------------------
 
@@ -108,7 +158,7 @@ INSERT INTO `quote` (`id`, `name`, `email`, `subject`, `msg`) VALUES
 --
 
 CREATE TABLE `social_links` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(50) NOT NULL,
   `icon` text DEFAULT NULL,
   `preview` text DEFAULT NULL,
   `platform` text DEFAULT NULL,
@@ -120,7 +170,7 @@ CREATE TABLE `social_links` (
 --
 
 INSERT INTO `social_links` (`id`, `icon`, `preview`, `platform`, `link`) VALUES
-(8, 'hi', 'wrtxrycuv', 'yuo', 'ther'),
+(8, 'hiyo', 'wrtxrycuv', 'yuouuuo', 'thereyo'),
 (9, 'hi', 'zteeey', 'yuo', 'there');
 
 -- --------------------------------------------------------
@@ -130,7 +180,7 @@ INSERT INTO `social_links` (`id`, `icon`, `preview`, `platform`, `link`) VALUES
 --
 
 CREATE TABLE `subscribe` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(50) NOT NULL,
   `email` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -141,7 +191,8 @@ CREATE TABLE `subscribe` (
 INSERT INTO `subscribe` (`id`, `email`) VALUES
 (1, 'qwertyui'),
 (3, 'rahul'),
-(4, 'gggggggggggggg');
+(4, 'gggggggggggggg'),
+(6, 'admin@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -176,19 +227,38 @@ CREATE TABLE `web_config` (
   `address` text DEFAULT NULL,
   `location` text DEFAULT NULL,
   `logo` text DEFAULT NULL,
-  `message` text NOT NULL
+  `message` text NOT NULL,
+  `ab_line1` text NOT NULL,
+  `ab_line2` text NOT NULL,
+  `ab_line3` text NOT NULL,
+  `ab_line4` text NOT NULL,
+  `facebook` text NOT NULL,
+  `twitter` text NOT NULL,
+  `instagram` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `web_config`
 --
 
-INSERT INTO `web_config` (`id`, `email`, `phn`, `address`, `location`, `logo`, `message`) VALUES
-(1, 'yujjhngng@fs', 356789234, 'asdgjvsaygiy13t4453@23', 'lojxdjsjlm4y25', 'http://localhost/medical/admin/uploads/1614231339_vlcsnap-2021-01-16-01h36m26s504.png', 'we are a firm from uk vaagha consultancy.');
+INSERT INTO `web_config` (`id`, `email`, `phn`, `address`, `location`, `logo`, `message`, `ab_line1`, `ab_line2`, `ab_line3`, `ab_line4`, `facebook`, `twitter`, `instagram`) VALUES
+(1, 'contact@int-trade', 356789234, 'int-trade', 'lojxdjsjlm4y25', 'https://dubuddy.in/medical/admin/uploads/1614707339_400dpiLogo.png', 'We are reputed Supplier to GP Clinics, Carehomes and NHS Trusts.', 'Reputed Supplier to GP Clinics, Carehomes and NHS Trusts ', 'Reliable Supplier of Premium range of Incontinence Bed Pads & Mobility Consumables ', 'A Wide range of PPE Medical Grade Masks, Shoe Covers, Disposable Aprons, Mob Caps & Gloves', ' All products are manufactured in ISO 9001 & ISO 13485 Certified facilities', 'www.facebook', 'as', 'wsds');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `home_slider`
+--
+ALTER TABLE `home_slider`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
@@ -237,34 +307,46 @@ ALTER TABLE `web_config`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `home_slider`
+--
+ALTER TABLE `home_slider`
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `product_img`
 --
 ALTER TABLE `product_img`
-  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `quote`
 --
 ALTER TABLE `quote`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `social_links`
 --
 ALTER TABLE `social_links`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `subscribe`
 --
 ALTER TABLE `subscribe`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
